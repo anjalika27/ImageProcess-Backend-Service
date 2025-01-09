fafbefc518ea4ada8329eb12216d15f52f373cc5
Anjalika <anjalika.a@yelloskye.com>
2025-01-09 19:18:34 +0530

    enabled addJob instead of calling processImage

--- controller/job.js
+++ controller/job.js
@@ -1,71 +1,79 @@
 import { processImage } from "../helpers/processImage.js";
-import pool from "../model/db.js"
+import pool from "../model/db.js";
 import { addJob } from "../services/jobProcessor.js";
 
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 export async function submitJob(req, res) {
-    // // console.log('req processing');
-    // // await addJob('1',['a','b']);
-    // const response = await axios({ url: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Chain_link_icon_slanted.png', responseType: 'arraybuffer' });
-    // const buffer = Buffer.from(response.data);
+  // // console.log('req processing');
+  // // await addJob('1',['a','b']);
+  // const response = await axios({ url: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Chain_link_icon_slanted.png', responseType: 'arraybuffer' });
+  // const buffer = Buffer.from(response.data);
 
-    // // Using sharp to get metadata
-    // const metadata = await sharp(buffer).metadata();
-    // return res.send(metadata);
-    const { count, visits } = req.body;
-    if (!count || !visits || count !== visits.length) {
-        return res.status(400).json({
-            error: 'Invalid/Empty input fields'
-        })
-    }
+  // // Using sharp to get metadata
+  // const metadata = await sharp(buffer).metadata();
+  // return res.send(metadata);
+  const { count, visits } = req.body;
+  if (!count || !visits || count !== visits.length) {
+    return res.status(400).json({
+      error: "Invalid/Empty input fields",
+    });
+  }
 
-    try {
-        const jobId = uuid();
-        const data = await pool.query('insert into jobs values($1,$2,$3)', [jobId, 'Ongoing', new Date().toISOString()]);
-        // await addJob(jobId, visits);
+  try {
+    const jobId = uuid();
+    const data = await pool.query("insert into jobs values($1,$2,$3)", [
+      jobId,
+      "Ongoing",
+      new Date().toISOString(),
+    ]);
+    await addJob(jobId, visits);
 
-        const process = await processImage(jobId, visits);
-        
-        return res.status(200).json({
-            process,
-            jobId
-        })
-    } catch (error) {
-        return res.status(500).json({
-            error: 'Internal server error'
-        })
-    }
+    // const process = await processImage(jobId, visits);
+
+    return res.status(200).json({
+      process,
+      jobId,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
 }
 
 export async function getJobStatus(req, res) {
-    const { jobId } = req.query;
-    if (!jobId) return res.status(400).json({ error: 'pass jobId as a query parameter' });
-    try {
-        const jobData = await pool.query('select * from jobs where "id"=($1)', [String(jobId)]);
-        if (jobData.rows.length === 0) return res.status(400).json({ error: `Can't find job with ID ${jobId}` })
-        else {
-            const data = jobData.rows[0];
-            if (data.status === 'Completed') return res.status(200).json({
-                status: 'Completed',
-                jobId
-            })
-            else if (data.status === 'Failed') {
-                return res.status(200).json({
-                    status: "Failed",
-                    jobId,
-                    error: "job failed"
-                })
-            }
-            return res.status(200).json({
-                status: 'Ongoing',
-                jobId
-            })
-        }
-    } catch (error) {
-        return res.status(404).json({
-            error: 'Bad Request'
-        })
+  const { jobId } = req.query;
+  if (!jobId)
+    return res.status(400).json({ error: "pass jobId as a query parameter" });
+  try {
+    const jobData = await pool.query('select * from jobs where "id"=($1)', [
+      String(jobId),
+    ]);
+    if (jobData.rows.length === 0)
+      return res.status(400).json({ error: `Can't find job with ID ${jobId}` });
+    else {
+      const data = jobData.rows[0];
+      if (data.status === "Completed")
+        return res.status(200).json({
+          status: "Completed",
+          jobId,
+        });
+      else if (data.status === "Failed") {
+        return res.status(200).json({
+          status: "Failed",
+          jobId,
+          error: "job failed",
+        });
+      }
+      return res.status(200).json({
+        status: "Ongoing",
+        jobId,
+      });
     }
+  } catch (error) {
+    return res.status(404).json({
+      error: "Bad Request",
+    });
+  }
 }
-
