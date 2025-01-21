0ca375b19ae96c6d32eee90611857e54f1081420
Anjalika Agarwal <112751110+anjalika27@users.noreply.github.com>
2025-01-21 12:17:27 +0530

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,12 +1,16 @@
-import pkg from "pg"
-const {Pool}=pkg
+import pkg from "pg";
+import dotenv from "dotenv";
 
-const pool=new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'store',
-    password:'12345',
-    port:5432,
-})
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
 
 export default pool;
